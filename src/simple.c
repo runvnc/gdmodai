241a24500cd996032e292adb77f6fb827de97936
Jason Livesay <runvnc@gmail.com>
2020-09-08 20:26:32 +1300

    build.sh

--- src/simple.c
+++ src/simple.c
@@ -5,6 +5,14 @@
 const godot_gdnative_core_api_struct *api = NULL;
 const godot_gdnative_ext_nativescript_api_struct *nativescript_api = NULL;
 
+void *simple_constructor(godot_object *p_instance, void *p_method_data);
+
+void simple_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
+
+godot_variant simple_get_data(godot_object *p_instance, void *p_method_data,
+        void *p_user_data, int p_num_args, godot_variant **p_args);
+  
+
 void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options) {
     api = p_options->api_struct;
 
