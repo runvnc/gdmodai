ef1f6e1622de1553e4bd0ab685e1a79cc486d12e
root <runvnc@gmail.com>
2020-09-16 02:12:11 -0700

    updates

--- src/simple.c
+++ src/simple.c
@@ -14,74 +14,81 @@ void *simple_constructor(godot_object *p_instance, void *p_method_data);
 void simple_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
 
 godot_variant simple_get_data(godot_object *p_instance, void *p_method_data,
-        void *p_user_data, int p_num_args, godot_variant **p_args);
+		void *p_user_data, int p_num_args, godot_variant **p_args);
 
 void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options);
    
  
 void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options) {
-    api = p_options->api_struct;
+	api = p_options->api_struct;
 
-    // Now find our extensions.
-    for (int i = 0; i < api->num_extensions; i++) {
-        switch (api->extensions[i]->type) {
-            case GDNATIVE_EXT_NATIVESCRIPT: {
-                nativescript_api = (godot_gdnative_ext_nativescript_api_struct *)api->extensions[i];
-            }; break;
-            default: break;
-        }
-    }
+	// Now find our extensions.
+	for (int i = 0; i < api->num_extensions; i++) {
+		switch (api->extensions[i]->type) {
+			case GDNATIVE_EXT_NATIVESCRIPT: {
+				nativescript_api = (godot_gdnative_ext_nativescript_api_struct *)api->extensions[i];
+			}; break;
+			default: break;
+		}
+	}
 }
 
 void GDN_EXPORT godot_nativescript_init(void *p_handle) {
-    godot_instance_create_func create = { NULL, NULL, NULL };
-    create.create_func = &simple_constructor;
+	godot_instance_create_func create = { NULL, NULL, NULL };
+	create.create_func = &simple_constructor;
 
-    godot_instance_destroy_func destroy = { NULL, NULL, NULL };
-    destroy.destroy_func = &simple_destructor;
+	godot_instance_destroy_func destroy = { NULL, NULL, NULL };
+	destroy.destroy_func = &simple_destructor;
 
-    nativescript_api->godot_nativescript_register_class(p_handle, "Simple", "Reference",
-            create, destroy);
+	nativescript_api->godot_nativescript_register_class(p_handle, "Simple", "Reference",
+			create, destroy);
 
-    godot_instance_method get_data = { NULL, NULL, NULL };
-    get_data.method = &simple_get_data;
+	godot_instance_method get_data = { NULL, NULL, NULL };
+	get_data.method = &simple_get_data;
 
-    godot_method_attributes attributes = { GODOT_METHOD_RPC_MODE_DISABLED };
+	godot_method_attributes attributes = { GODOT_METHOD_RPC_MODE_DISABLED };
 
-    nativescript_api->godot_nativescript_register_method(p_handle, "Simple", "get_data",
-            attributes, get_data);
+	nativescript_api->godot_nativescript_register_method(p_handle, "Simple", "get_data",
+			attributes, get_data);
 }
 
 typedef struct user_data_struct {
-    char data[256];
+	char data[256];
 } user_data_struct;
 
 
 void *simple_constructor(godot_object *p_instance, void *p_method_data) {
-    user_data_struct *user_data = (user_data_struct*)api->godot_alloc(sizeof(user_data_struct));
-    strcpy(user_data->data, "Hello there.!");
-    init();
-    return user_data;
+	user_data_struct *user_data = (user_data_struct*)api->godot_alloc(sizeof(user_data_struct));
+	strcpy(user_data->data, "Hello there.!");
+	init();
+	return user_data;
 }
 
 void simple_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data) {
-    api->godot_free(p_user_data);
-    done();
+	api->godot_free(p_user_data);
+	done();
 }
 
 godot_variant simple_get_data(godot_object *p_instance, void *p_method_data,
-        void *p_user_data, int p_num_args, godot_variant **p_args) {
+		void *p_user_data, int p_num_args, godot_variant **p_args) {
 
-    godot_string data;
-    godot_variant ret;
-    //user_data_struct *user_data = (user_data_struct *)p_user_data;
+	godot_array inp;
+	inp = godot_variant_as_array(p_args[0]);
+	int64_t[100*100] img;
+	int64_t color;
+	for (int i=0; i<100*100; i++) {
+		img[i] = godot_variant_as_int(godot_array_get(inp, i))
+	}
+	godot_string data;
+	godot_variant ret;
+	//user_data_struct *user_data = (user_data_struct *)p_user_data;
 
-    api->godot_string_new(&data);
-    api->godot_string_parse_utf8(&data, calc() );//user_data->data);
-    api->godot_variant_new_string(&ret, &data);
-    api->godot_string_destroy(&data);
+	api->godot_string_new(&data);
+	api->godot_string_parse_utf8(&data, calc() );
+	api->godot_variant_new_string(&ret, &data);
+	api->godot_string_destroy(&data);
 
-    return ret;
+	return ret;
 }
 
 
