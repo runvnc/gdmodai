a329e588efb0fc07af4f17776a01d2c737670266
root <runvnc@gmail.com>
2020-10-22 21:51:49 -0700

    update

--- simple/spatial.gd
+++ simple/spatial.gd
@@ -7,6 +7,8 @@ var ly = 250
 var img
 var imgtext
 
+var lastpos
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,14 +16,16 @@ var imgtext
 # load the Simple library
 onready var data = preload("res://bin/simple.gdns").new()
 
-func _on_Button_pressed():
+#func _on_Button_pressed():
+func look():
 	img = Image.new()
 	img.create(100,100,false,Image.FORMAT_RGBA8)
 	var imgdata = []
 	var image = get_viewport().get_texture().get_data()
 	image.lock()
 	img.lock()
-
+	lx = lastpos.x
+	ly = get_viewport().size.y - lastpos.y
 	for j in range(0,looksize):
 		for i in range(0,looksize):
 			var color = image.get_pixel(lx+i,ly+j)
@@ -34,7 +38,6 @@ func _on_Button_pressed():
 	
 	img.lock()
 	var arr = data.get_data(imgdata)
-	print(arr[0])
 	
 	for j in range(0,looksize):
 		for i in range(0,looksize):
@@ -47,6 +50,17 @@ func _on_Button_pressed():
 	var view = get_tree().current_scene.find_node("view")
 	view.texture = imgtext
 
+func _input(event):
+	if event is InputEventMouseButton:
+		#print("Mouse Click/Unclick at: ", event.position)
+		look()
+	elif event is InputEventMouseMotion:
+		lastpos = event.position
+		look()
+		#print("Mouse Motion at: ", event.position)
+
+   # Print the size of the viewport.
+   #print("Viewport Resolution is: ", get_viewport_rect().size)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
